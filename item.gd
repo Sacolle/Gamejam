f2527f49dbac5d10a2303531b1f7c2d6a10cd54e
Pedro Colle <pedro.h.b.colle@gmail.com>
2024-03-24 21:54:09 -0300

    modificações

--- item.gd
+++ item.gd
@@ -14,7 +14,6 @@ var is_rotating = false
 @onready
 var label = $TimeScanned
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
