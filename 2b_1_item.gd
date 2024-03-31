47e5a1a7a79ebc4ebaab8887947acb48d02203a2
Pedro Colle <pedro.h.b.colle@gmail.com>
2024-03-31 16:25:56 -0300

    fazendo funcionar a rotação e colisão do 2x1, faltando agora soh fazer com  q um lado escaneie

--- 2b_1_item.gd
+++ 2b_1_item.gd
@@ -12,11 +12,16 @@ var time: float = 0
 var is_hit = false
 var is_rotating = false
 
+enum Collision { waiting, yes, no }
+
+var collided: Collision = Collision.no
+
 @onready var hit_box = $hitBox
-@onready var point = $Center
+@onready var ray = $hitBox/CollisionRay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ray.add_exception(self)
 	pass # Replace with function body.
 
 func start(pos):
@@ -38,26 +43,27 @@ func set_item_center(cell: Vector2i) -> bool:
 	if cell.x == position.x and cell.y == position.z: 
 		return false
 	var old_pos = Vector2(position.x, position.z)
-	print("Changed center from ", old_pos, " to ", cell)
-	#puts the position at the cell
-	#hit_box.position += $A.position - $B.position
-	
+
 	position.x = cell.x
 	position.z = cell.y
 	
-	
 	#offsets the hitbox and mesh based on current orientation
 	var orientation = current_orientation()
-	var shape_dim = Vector2(2.0, hit_box.shape.size.z)
+	var shape_dim = Vector2(hit_box.shape.size.x, hit_box.shape.size.z)
+	var offset = 0
 	match orientation:
-		0: hit_box.position.x = float(old_pos.x - cell.x) / shape_dim.x
-		1: hit_box.position.x = float(cell.y - old_pos.y) / shape_dim.x
-		2: hit_box.position.x = float(cell.x - old_pos.x) / shape_dim.x #reversed 0
-		3: hit_box.position.x = float(old_pos.y - cell.y) / shape_dim.x #reversed 1
+		0: offset = float(old_pos.x - cell.x)
+		1: offset = float(cell.y - old_pos.y)
+		2: offset = float(cell.x - old_pos.x) #reversed 0
+		3: offset = float(old_pos.y - cell.y) #reversed 1
 	
-
+	hit_box.position.x = offset / shape_dim.x
+	
+	#print("hitbox offset:", hit_box.position)
+	#print('')
 	return true
 
+"""
 func girar(amount: float):
 	is_rotating = true
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
@@ -67,9 +73,10 @@ func girar(amount: float):
 func is_not_rotating():
 	is_rotating = false
 	#reset the atributes
+"""
 
 func current_orientation() -> int:
-	return (int(rotation.y / (PI/2)) + 4) % 4
+	return (int(round(rotation.y / (PI/2))) + 4) % 4
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -83,31 +90,37 @@ func _process(delta):
 		if time < 0:
 			time = 0
 
-@onready var left_ray = $hitBox/LeftRay
-@onready var right_ray = $hitBox/RightRay
 #
-func is_rotation_valid(direction: int, changed_cell: bool) -> bool:
-	#if the orientation is reversed, reversed the suposed direction
-	#isso eh um hack fenomenal, mas fdc
-	if changed_cell:
-		var tmp = left_ray
-		left_ray = right_ray
-		right_ray = tmp
-		
-	if direction == 1:
-		return not left_ray.is_colliding()
-	else:
-		return not right_ray.is_colliding()
+func is_rotation_valid(direction: int) -> bool:
+	#ray.position = position
+	ray.rotation.y += direction * (PI/2)
+	ray.force_raycast_update()
+	var is_colliding = ray.is_colliding()
+	ray.rotation.y = 0
+	return not is_colliding
+
+
 
 func _on_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and event.is_pressed():
 		var selected_cell = Grid.world_to_grid(position)
 		var local_cell = Grid.world_to_grid(position - self.position)
-		print(position)
-		print(selected_cell)
-		print("Local", local_cell)
+		"""
+		if not (local_cell.x == 0 and local_cell.y == 0):
+			print("posição", position)
+			print("celula selecionada", selected_cell)
+			print("Local", local_cell)
+			print("orientação:", current_orientation())
+			print("rotação: ", (rotation.y / PI) * 180)
+		"""
 		if not is_rotating:
 			var changed_cell = set_item_center(selected_cell)
 			var dir = 1 if event.button_index == MOUSE_BUTTON_LEFT else -1 
-			if is_rotation_valid(dir, changed_cell):
-				girar(dir)
+			var old_rotation = rotation.y 
+			
+			#jeito mais mongol de se fazer, mas fazer oq
+			rotation.y += dir * (PI/2)
+			ray.force_raycast_update()
+			if ray.is_colliding():
+				rotation.y = old_rotation
+
