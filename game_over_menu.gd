1a31884dff28f24471415d4eeaa70d637190a182
yasmin beer <55171112+beersprite@users.noreply.github.com>
2024-04-03 17:56:08 -0300

    created game over screen

---
+++ game_over_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_carregar_btn_pressed():
+	pass # Replace with function body.
+	
+func _on_sair_btn_pressed():
+	get_tree().quit()
